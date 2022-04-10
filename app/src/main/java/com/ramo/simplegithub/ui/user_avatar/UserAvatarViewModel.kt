9a4cc66f3f92908bf50d2b84717ac456e42b7e56
Ramazan Öğünç <ramazanogunc@hotmail.com>
2022-04-10 17:16:04 +0300

    Delete unused function

--- app/src/main/java/com/ramo/simplegithub/ui/user_avatar/UserAvatarViewModel.kt
+++ app/src/main/java/com/ramo/simplegithub/ui/user_avatar/UserAvatarViewModel.kt
@@ -1,27 +1,8 @@
 package com.ramo.simplegithub.ui.user_avatar
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.ramo.simplegithub.core.BaseViewModel
-import com.ramo.simplegithub.domain.model.User
-import com.ramo.simplegithub.domain.usecase.UserDetailUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class UserAvatarViewModel @Inject constructor(
-    private val userDetailUseCase: UserDetailUseCase
-) : BaseViewModel() {
-
-    private val _user = MutableLiveData<User>()
-    val user: LiveData<User> = _user
-
-    fun getUserDetail(userName: String) = safeScope {
-        _user.value = userDetailUseCase.invoke(userName)
-    }
-
-
-    fun changeFavoriteStatus(user: User) {
-        // TODO:
-    }
-}
+class UserAvatarViewModel @Inject constructor() : BaseViewModel()
